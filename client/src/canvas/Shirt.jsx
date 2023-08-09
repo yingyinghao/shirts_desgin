ae1acd3196ddb0c591189a799daedb086f446283
Yingying <92928939+yingyinghao@users.noreply.github.com>
2023-08-09 11:52:58 -0400

    backdrop

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -13,9 +13,15 @@ const Shirt = () => {
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
+  useFrame((state, delta) =>
+    easing.dampC(materials.lamber1.color,
+      snap.color, 0.25, delta));
+
+      const stateString = JSON.stringify(snap);
+
 
   return (
-    <group>
+    <group key = {stateString}>
       <mesh
       castShadow
       geometry={nodes.T_Shirt_male.geometry}
@@ -23,6 +29,25 @@ const Shirt = () => {
       material-roughtness = {1}
       dispose={null}
       >
+        {snap.isFullTexture && (
+          <Decal
+          position = {[0, 0, 0]}
+          rotation = {[0, 0, 0]}
+          scale = {1}
+          map = {fullTexture}
+          />
+        )}
+        {snap.isLogoTexture && (
+        <Decal
+        position = {[0, 0.04, 0.15]}
+        rotation = {[0, 0, 0]}
+        scale = {0.15}
+        map = {logoTexture}
+        map-anisotropy = {16}
+        depthTest = {false}
+        depthwrite = {true}
+        />
+        )}
       </mesh>
       </group>
     )
