93e5469f16b028476bcfd169c5ca9199fe0e36b7
Yingying <92928939+yingyinghao@users.noreply.github.com>
2023-08-10 22:51:33 -0400

    tab

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import {useSnapshot} from 'valtio'
+
+import state from '../store'
+
+const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) =>
+{
+  const snap = useSnapshot(state)
+
 
 const Tab = () => {
   return (
