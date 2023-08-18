5f0d9bed5ad091d866b753513a875800ce4869cd
Yingying <92928939+yingyinghao@users.noreply.github.com>
2023-08-17 23:12:27 -0400

    filePicker

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import CustomButton from './CustomButton'
+
 
 const FilePicker = () => {
   return (
-    <div>FilePicker</div>
-  )
+    <div className="filepicker-container">
+      <div className='flex-1 flex flex-col'
+      <input
+      id="file-upload"
+      type="file"
+      accept="image/*"
+      onChange={(e) => setFile(e.target.files[0])}
+      />
+      <label htmlFor='file-upload' className='filepicker-label'>
+        Upload Files
+      </label>
+
+    <p className='>
+    {file === "" ? "No file selected" : file.name}
+    </p>
+      </div>
+      </div>
+    )
 }
 
 export default FilePicker
